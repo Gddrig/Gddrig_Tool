4d00c6f8bba8a1b27ab786d8b54b2804244a31a6
Gddrig <123659475+Gddrig@users.noreply.github.com>
2023-04-30 21:24:20 +0200

    Update script.sh

--- test/script.sh
+++ test/script.sh
@@ -14,11 +14,11 @@ NC='\033[0m'
 
 function start_script1() {
 
-		#·TCC
+		#TCC
 		echo -e "${GREEN}Minage Taichicoin ${NC}"
 		nvtool --setclocks 1200 --setmem 5000 & /hive/miners/wildrig-multi/0.36.6/wildrig-multi --algo=ghostrider --url aidepremium.ddns.net:3056 --user TdaZ1bcksSFyK5uY44njUyZY9kETq4TqwE.Gddrig --pass x & pid1=$!
         echo -e "pid = $pid1"
-		sleep 180
+		sleep 600
 		#kill -9 $pid1
 		kill -9 $(pgrep wildrig-multi)
 		echo -e "${RED}Minage Taichicoin terminé !${NC}"
@@ -30,7 +30,7 @@ function start_script2() {
 		echo -e "${GREEN}Minage Bitfishcoin ${NC}"
 		nvtool --setclocks 1200 --setmem 5000 & /hive/miners/wildrig-multi/0.36.6/wildrig-multi --algo=ghostrider --url aidepremium.ddns.net:3053 --user BbZ17LWBHZTmWEnQgQ3JaKzRsuyj1NyjMd.Gddrig --pass x & pid2=$!
         echo -e "pid = $pid2"
-		sleep 180
+		sleep 600
 		#kill -9 $pid2
 		kill -9 $(pgrep wildrig-multi)
 		echo -e "${RED}Minage Bitfishcoin terminé !${NC}"
@@ -42,12 +42,11 @@ function start_script3() {
 		echo -e "${GREEN}Minage Litpreum ${NC}"
 		nvtool --setclocks 1200 --setmem 5000 & /hive/miners/wildrig-multi/0.36.6/wildrig-multi --algo=ghostrider --url aidepremium.ddns.net:3054 --user LVpxKRTjRZKeM6WDkhtNASnZimhMHqNu5M.Gddrig --pass x & pid3=$!
         echo -e "pid = $pid3"
-		sleep 180
+		sleep 600
 		#kill -9 $pid3
 		kill -9 $(pgrep wildrig-multi)
 		echo -e "${RED}Minage Litoreum terminé !${NC}"
 		sleep 5
-		exit 0
 		       
 }
 
@@ -65,8 +64,12 @@ echo -e "${GREEN}Version: $dversion ${NC}"
 echo -e ""
 echo -e "${GREEN}Démarrage du script : ${NC}"
 
+
+for ((i = 0; i < 1000; i++ ))
+do
 start_script1
 start_script2
 start_script3
+done
 
 exit 0
